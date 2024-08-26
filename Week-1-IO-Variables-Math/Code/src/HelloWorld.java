ec4e73a98c163f37fe7a86d330eb8fcde5982148
DESKTOP-64L3K3P\Clarissa <milligan.26@wright.edu>
2024-08-26 19:44:45 -0400

    added comments and variable intro

--- Week-1-IO-Variables-Math/Code/src/HelloWorld.java
+++ Week-1-IO-Variables-Math/Code/src/HelloWorld.java
@@ -1,8 +1,22 @@
-
+/** @author Clarissa
+ *
+ */
 public class HelloWorld
 {
+    /*
+    main method does x
+    y
+    and z
+     */
     public static void main(String[] args)
     {
+        int number;
+        number = 5;
+        System.out.println(number);
+        number = 2;
+        System.out.println(number);
+
+        // Prints to console
         System.out.println("Hello World! Welcome to CS-1180!!");
     }
 }
