9195672e889a815c202a6d10d86880763c058277
DESKTOP-64L3K3P\Clarissa <milligan.26@wright.edu>
2024-08-29 00:10:09 -0400

    Added comments to code

--- Week-1-IO-Variables-Math/Code/src/HelloWorld.java
+++ Week-1-IO-Variables-Math/Code/src/HelloWorld.java
@@ -1,3 +1,4 @@
+
 /** @author Clarissa
  *
  */
@@ -7,16 +8,27 @@ public class HelloWorld
     main method does x
     y
     and z
-     */
+    */
     public static void main(String[] args)
     {
-        int number;
-        number = 5;
-        System.out.println(number);
-        number = 2;
-        System.out.println(number);
-
         // Prints to console
-        System.out.println("Hello World! Welcome to CS-1180!!");
+        System.out.println("Hello World! Welcome to CS-1180!!"); // "say hi"
+
+        // Notice that the value of 'num' changes to 6 after we set num
+        // equal to 6 with the assignment operator
+        int num = 5;
+        System.out.print(num);
+        num = 6;
+        System.out.println(num);
+
+        // Demonstrates calling a method on an object
+        // An object is an instance of a class
+        String randomLetters = "AbCdEghIj";
+
+        // toLowerCase() returns the lowercase version of the String
+        // We need to assign this value back to our original String
+        // in order to save the lowercase version to the variable
+        randomLetters = randomLetters.toLowerCase();
+        System.out.println(randomLetters);
     }
 }
