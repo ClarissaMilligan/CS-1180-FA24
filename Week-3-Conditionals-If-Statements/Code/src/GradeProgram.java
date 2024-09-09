8014ae74cf71531148cd4b45209569d144c7169a
DESKTOP-64L3K3P\Clarissa <milligan.26@wright.edu>
2024-09-09 19:13:33 -0400

    In-class activity solution

--- Week-3-Conditionals-If-Statements/Code/src/GradeProgram.java
+++ Week-3-Conditionals-If-Statements/Code/src/GradeProgram.java
@@ -15,5 +15,71 @@ public class GradeProgram
         System.out.println("Input grade on 3rd assignment (0-100): ");
         double grade3 = input.nextDouble();
 
+        if (grade1 >= 89.5)
+        {
+            System.out.println("Grade 1: A");
+        }
+        else if (grade1 >= 79.5)
+        {
+            System.out.println("Grade 1: B");
+        }
+        else if (grade1 >= 69.5)
+        {
+            System.out.println("Grade 1: C");
+        }
+        else if (grade1 >= 59.5)
+        {
+            System.out.println("Grade 1: D");
+        }
+        else
+        {
+            System.out.println("Grade 1: F");
+        }
+
+        if (grade2 >= 89.5)
+        {
+            System.out.println("Grade 2: A");
+        }
+        else if (grade2 >= 79.5)
+        {
+            System.out.println("Grade 2: B");
+        }
+        else if (grade2 >= 69.5)
+        {
+            System.out.println("Grade 2: C");
+        }
+        else if (grade2 >= 59.5)
+        {
+            System.out.println("Grade 2: D");
+        }
+        else
+        {
+            System.out.println("Grade 2: F");
+        }
+
+        if (grade3 >= 89.5)
+        {
+            System.out.println("Grade 3: A");
+        }
+        else if (grade3 >= 79.5)
+        {
+            System.out.println("Grade 3: B");
+        }
+        else if (grade3 >= 69.5)
+        {
+            System.out.println("Grade 3: C");
+        }
+        else if (grade3 >= 59.5)
+        {
+            System.out.println("Grade 3: D");
+        }
+        else
+        {
+            System.out.println("Grade 3: F");
+        }
+
+        double avgGrade = (grade1 + grade2 + grade3) / 3;
+        System.out.printf("The average grade is: %.2f", avgGrade);
+
     }
 }
