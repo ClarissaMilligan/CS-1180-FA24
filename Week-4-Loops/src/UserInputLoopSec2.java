a7c59a9b882b998e15cc91bebaad6b556123168c
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2024-09-23 13:17:06 -0400

    added comments

--- Week-4-Loops/src/UserInputLoopSec2.java
+++ Week-4-Loops/src/UserInputLoopSec2.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class UserInputLoop
+public class UserInputLoopSec2
 {
     public static void main(String[] args)
     {
         Scanner scnr = new Scanner(System.in);
 
+        // loop will essentially loop forever, since next can handle any input
         while(scnr.hasNext())
         {
+            // this line could still throw an error because we have only checked that there is data input,
+            // but have not verified if the user entered an integer
             int num = scnr.nextInt();
             System.out.println(num);
 
+            // repeats what the user typed
             String userInput = scnr.next();
             System.out.println(userInput);
 
